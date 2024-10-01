e096565f2046b9c5315e5b302c3573f3ec61c618
SelvinPL <przemek.sulikowski@gmail.com>
2024-10-01 22:58:56 +0200

    back to no_std :D

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//#![no_std]
+#![no_std]
 #![no_main]
 #![windows_subsystem = "windows"]
 
@@ -14,8 +14,7 @@ mod res;
 
 #[no_mangle]
 #[allow(non_snake_case)]
-//fn mainCRTStartup()
-fn main()
+fn mainCRTStartup()
 {
 	unsafe { ExitProcess(winmain::real_main() as u32); }
 }
--- src/panic.rs
+++ src/panic.rs
@@ -1,20 +1,20 @@
-// #[cfg(panic = "abort")]
-// mod panic 
-// {
-// 	use core::panic::PanicInfo;
-// 	use core::ptr::null_mut;
-// 	use windows_sys::core::{PCSTR, s};
-// 	use windows_sys::Win32::System::Threading::ExitProcess;
-// 	use windows_sys::Win32::UI::WindowsAndMessaging::*;
+#[cfg(panic = "abort")]
+mod panic 
+{
+	use core::panic::PanicInfo;
+	use core::ptr::null_mut;
+	use windows_sys::core::{PCSTR, s};
+	use windows_sys::Win32::System::Threading::ExitProcess;
+	use windows_sys::Win32::UI::WindowsAndMessaging::*;
 
-// 	#[panic_handler]
-// 	fn panic_handler(_: &PanicInfo<'_>) -> ! 
-// 	{
-// 		const PANIC: PCSTR = s!("fatal error");
-// 		unsafe 
-// 		{
-// 			MessageBoxA(null_mut(), PANIC, PANIC, MB_ICONERROR);
-// 			ExitProcess(1);
-// 		}
-// 	}
-// }
+	#[panic_handler]
+	fn panic_handler(_: &PanicInfo<'_>) -> ! 
+	{
+		const PANIC: PCSTR = s!("fatal error");
+		unsafe 
+		{
+			MessageBoxA(null_mut(), PANIC, PANIC, MB_ICONERROR);
+			ExitProcess(1);
+		}
+	}
+}
